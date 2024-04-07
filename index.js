9540c559bad76f5c81aa8a0fce2ec37836cd3b7f
jianggan <jianggan@baidu.com>
2024-04-07 21:36:59 +0800

    2024 4-7 21:37

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ module.exports = compose
  * a fully valid middleware comprised
  * of all those which are passed.
  *
- * @param {Array} middleware
+ * @param {Array} middleware middleware(中间件)数组
  * @return {Function}
  * @api public
  */
@@ -31,16 +31,23 @@ function compose (middleware) {
   return function (context, next) {
     // last called middleware #
     let index = -1
+    // 调用dispatch，传入0
     return dispatch(0)
     function dispatch (i) {
+      // i小于index，证明在中间件内调用了不止一次的next()，抛出错误
       if (i <= index) return Promise.reject(new Error('next() called multiple times'))
+      // 更新index的值
       index = i
+      // middleware中的函数，从第1个开始
       let fn = middleware[i]
+      // 如果i走到最后一个的后面，就让fn为next,此时fn为undefined
       if (i === middleware.length) fn = next
       if (!fn) return Promise.resolve()
       try {
+        // 将其包装成一个Promise resolve态，主要作用是区分reject
         return Promise.resolve(fn(context, dispatch.bind(null, i + 1)))
       } catch (err) {
+        // catch错误，并reject
         return Promise.reject(err)
       }
     }
